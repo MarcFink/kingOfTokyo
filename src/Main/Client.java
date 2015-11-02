cc1fe02c560a029f61d22db83aab251f0c149692
David <David@David.edu.ds.fhnw.ch>
2015-11-02 15:17:42 +0100

    df

--- src/Main/Client.java
+++ src/Main/Client.java
@@ -1,41 +1,29 @@
 package Main;
 
+import java.io.IOException;
+
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 public class Client extends Application {
-    private ClientView view;
-    private ClientController controller;
-    private ClientModel model;
+	@Override
+    public void start(Stage primaryStage) {
+        try {
+            // Load view.
+            BorderPane root =(BorderPane)FXMLLoader.load(getClass().getResource("../KingOfTokyoView/ClientView.fxml"));
+            Scene scene=new Scene(root);
+            primaryStage.setScene(scene);
+            primaryStage.show();
+    
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
     public static void main(String[] args) {
         launch(args);
-    }
-
-    /**
-     * Note the dependencies between model, view and controller. Additionally,
-     * the view needs the primaryStage created by JavaFX.
-     */
-
-    @Override
-    public void start(Stage primaryStage) throws Exception {
-        // Initialize the GUI
-        model = new ClientModel();
-        view = new ClientView(primaryStage, model);
-        controller = new ClientController(model, view);
-
-        // Display the GUI after all initialization is complete
-        view.start();
-    }
-
-    /**
-     * The stop method is the opposite of the start method. It provides an
-     * opportunity to close down the program gracefully, when the program has
-     * been closed.
-     */
-    @Override
-    public void stop() {
-        if (view != null)
-            view.stop();
     }
 }
