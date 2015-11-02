74545302949dedfc2e5f2404b3e7557ab85441a4
Fink <marc.fink1@students.fhnw.ch>
2015-11-02 11:45:50 +0100

    Update

---
+++ src/kingOfTokyo/MainClass.java
@@ -0,0 +1,17 @@
+package kingOfTokyo;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class MainClass extends Application {
+
+	@Override
+	public void start(Stage primaryStage) {
+		
+		
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
